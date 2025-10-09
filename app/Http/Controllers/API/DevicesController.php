74d0adf38be515a701318fa4e69013e020268549
ahmadsofuwan <sofuwan62@gmail.com>
2025-10-09 22:13:59 +0700

    a

--- app/Http/Controllers/API/DevicesController.php
+++ app/Http/Controllers/API/DevicesController.php
@@ -35,6 +35,8 @@ class DevicesController extends Controller
        
 
        return response($script, 200)
-       ->header('Content-Type', 'text/plain');
+       ->header('Content-Type', 'text/plain')
+       ->header('Content-Type', 'text/plain; charset=utf-8')
+       ->header('Cache-Control', 'no-cache, no-store, must-revalidate');
     }
 }
